60702c776a0f707ece8e4df168150a874f01e729
ralphwSU <pi@hypatia>
2020-01-24 13:00:56 -0500

    Added some sanity checks to t2.R.

--- R/t2.R
+++ R/t2.R
@@ -57,3 +57,45 @@ nfiles <- function() {
    return (sum(x$n))
 }
 
+# Compute the number of stations per year using the metadata file.
+# Each row has a start and end date.  Use these to count the number of
+# stations that are available each year.
+#
+# Some stations that, according to the metadata, were in existence during
+# certain years may not have data recorded in the data set for those years.
+mdStationsPerYear <- function() {
+   files = list.files(path = dataDir) # Assume this contains only year files
+   firstYear = 1901
+   lastYear  = as.integer(format(Sys.time(), format="%Y"))
+   years     = seq(firstYear, lastYear)   
+   number    = rep(0, length(years))
+   # Get the first year in the data set
+   # Get the last year in the data set
+   md = read_metadata()
+   n = dim(md)[1]
+   for (i in 1:n) {
+      startYear = as.integer(substr(md$BEGIN[i], 1, 4))
+      endYear   = as.integer(substr(md$END[i], 1, 4))
+      #print(paste(i, startYear, endYear))
+      for (year in seq(startYear, endYear)) {
+         if (year == 1910)
+	    print (paste(md$USAF[i], "-", md$WBAN[i], sep=''))
+         number[year - firstYear + 1] = number[year - firstYear + 1] + 1
+      }
+   }
+   return (data.frame("year"=years, "n"=number))   
+}
+
+# Are there any stations in the files that are not in the metadata
+# during the years the metadata specifies?
+sanity1 <- function() {
+   md = read_metadata()
+   years = list.files(path = dataDir)
+   for (year in years) {
+   #for each year
+   # list the files in the year directory
+   # extract the USAF and WBAN
+   # Does the row exist in md?
+   # Does the year occur in  the md range?
+   }
+}
